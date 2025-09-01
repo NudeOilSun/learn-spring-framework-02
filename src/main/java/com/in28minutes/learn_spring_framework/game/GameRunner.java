83e10f895d0cd6e81a395f0018e831a60fd0f364
NudeOilSun <94184352+NudeOilSun@users.noreply.github.com>
2025-09-01 11:32:37 +0100

    added qualifier

--- src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/GameRunner.java
@@ -1,12 +1,14 @@
 package com.in28minutes.learn_spring_framework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
     GamingConsole game;
 
-    public GameRunner(GamingConsole game){
+    //Qualifier directly wires up a specific component
+    public GameRunner(@Qualifier("SuperContraGameQualifier") GamingConsole game){
         this.game = game;
     }
 
--- src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
+++ src/main/java/com/in28minutes/learn_spring_framework/game/MarioGame.java
@@ -1,6 +1,10 @@
 package com.in28minutes.learn_spring_framework.game ;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+@Component
+@Primary //Gives this Component priority if multiple Components are found
 public class MarioGame implements GamingConsole {
     public void Up(){
         System.out.println("jump");
