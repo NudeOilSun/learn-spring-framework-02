40180ff879b3712e713b2800ec563be2b3f30e85
NudeOilSun <94184352+NudeOilSun@users.noreply.github.com>
2025-09-01 16:29:35 +0100

    consrctor injection

--- src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectorLauncherApplication.java
+++ src/main/java/com/in28minutes/learn_spring_framework/examples/a1/DependencyInjectorLauncherApplication.java
@@ -10,11 +10,27 @@ import java.util.Arrays;
 
 @Component
 class YourBussinessClass{
-    @Autowired
-    Dependency1 dependency1;
 
-    @Autowired
+    Dependency1 dependency1;
     Dependency2 dependency2;
+
+    //@Autowired - not mandatory for constructor injection
+    public YourBussinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter Injection - setDependency1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//        System.out.println("Setter Injection - setDependency2");
+//        this.dependency2 = dependency2;
+//    }
 
     public String ToString(){
         return "Using " + dependency1 + " and " + dependency2;
